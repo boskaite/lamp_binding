d8dda7b69b93287c901eebe0bda8ca469cbbf405
Бощенко Екатерина <84003765+boskaite@users.noreply.github.com>
2021-12-09 02:00:30 +0700

    Add files via upload

---
+++ client/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      message: "",
+    }
+  },
+  created: function () {
+    axios
+        .get('http://localhost:5000/about')
+        .then(response => {
+          this.message = response.data;
+        });
+  }
+}
+</script>
+
---
+++ client/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HelloWorld :msg="message"/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  el: 'HelloWorld',
+  name: 'Home',
+  data() {
+    return {
+      message: "",
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created: function() {
+    axios
+        .get('http://localhost:5000/')
+        .then(response => {
+          this.message = response.data;
+          console.log(response.data);
+        });
+    }
+  }
+</script>
